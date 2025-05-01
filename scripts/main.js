c9ad3cc45bbeddcfe42ef1624fe4d3a7ec8ba0ce
Ibrahima Cisse <ibrahimacisse.ic10@gmail.com>
2025-05-01 19:05:41 +0200

    Ajout des fichiers du projet AzerType

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
+ lancerJeu()
+
+/*let inputEcriture= document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot= document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let divZoneProposition= document.querySelector(".zoneProposition")
+console.log(divZoneProposition)
+
+let spanScore= document.querySelector(".zoneScore span")
+console.log(spanScore)
+
+let listeBtnRadio= document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)*/
+
